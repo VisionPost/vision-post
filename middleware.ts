fb2cfcfa49523cef35a808e098f0fb68632ff762
Govind Vashishat <govindvashishat@gmail.com>
2025-02-06 00:00:44 +0530

    added routes

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,11 @@ import { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req });
     console.log(token);
+    const url = req.nextUrl;
+    console.log(url);
 }
 
 export const config = {
-     matcher: [
-        '/',
-     ]   
-}
+    matcher: ['/'],
+};
+ 

fba1b6da4a4010213d5676ca5d3dbe5a091ef2ad
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-02-18 15:39:38 +0530

    improved error handling

--- app/onboarding/actions.ts
+++ app/onboarding/actions.ts
@@ -4,22 +4,33 @@ import { revalidatePath } from "next/cache";
 import { prisma } from "../lib/db";
 import { redirect } from "next/navigation";
 import { getAuthUser } from "../lib/authHelpers";
+import { Prisma } from "@prisma/client";
 
 export async function updateStep(step: number) {
+    try {
     const user = await getAuthUser();
 
     await prisma.user.update({
-        where: { id: user.id },
-        data: {
-            onBoardingStep: step,
-        },
+    where: { id: user.id },
+    data: {
+        onBoardingStep: step,
+       },
     });
 
     revalidatePath("/onboarding");
+    } catch (e) {
+        if(e instanceof Prisma.PrismaClientKnownRequestError) {
+            console.error("Database Error", e);
+            redirect('/error');
+        };
+        console.error("Internal server error", e);
+        redirect('/error');
+    };
     redirect(`/onboarding/${step}`);
 };
 
 export async function storeTwitterUserName(twitterUsername: string, step: number) {
+    try {
     const user = await getAuthUser();
 
     await prisma.user.update({
@@ -31,10 +42,19 @@ export async function storeTwitterUserName(twitterUsername: string, step: number
     });
 
     revalidatePath("/onboarding");
+    } catch (e) {
+        if(e instanceof Prisma.PrismaClientKnownRequestError) {
+            console.error("Database Error", e);
+            redirect('/error');
+        };
+        console.error("Internal server error", e);
+        redirect('/error');
+    };
     redirect(`/onboarding/${step}`);
 };
 
 export async function completeOnboarding() {
+    try {
     const user = await getAuthUser();
 
     await prisma.user.update({
@@ -45,5 +65,13 @@ export async function completeOnboarding() {
     });
 
     revalidatePath('/dashboard');
+    } catch (e) {
+        if(e instanceof Prisma.PrismaClientKnownRequestError) {
+            console.error("Database Error", e);
+            redirect('/error');
+        };
+        console.error("Internal server error", e);
+        redirect('/error');
+    };
     redirect("/dashboard");
 };

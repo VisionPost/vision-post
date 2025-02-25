de0b1540a243d4f2f11d70166adca0db935d3949
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-02-26 02:15:24 +0530

    added ui for onboarding step two

--- app/components/StepTwo.tsx
+++ app/components/StepTwo.tsx
@@ -3,6 +3,10 @@
 import { useState } from "react";
 import { storeTwitterUserName } from "../onboarding/actions";
 import { useRouter } from "next/navigation";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { FaXTwitter } from "react-icons/fa6";
+import { ArrowRight } from "lucide-react";
 
 export default function StepTwo() {
     const [twitterUsername, setTwitterUsername] = useState<string>("");
@@ -21,19 +25,37 @@ export default function StepTwo() {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <h1 className="mt-10">Enter your X usernmame:</h1>
-            <input 
-            type="text" 
-            value={twitterUsername}
-            onChange={(e) => setTwitterUsername(e.target.value)}
-            placeholder="@yourtwitterusername"
-            className="text-black mt-2"
-            />
-            {error && <p className="text-red-500 mt-2">{error}</p>}
+            <div className="text-center mb-6 space-y-6">
+            <h1 className="text-5xl md:text-6xl font-bold text-white">Connect your Twitter</h1>
+            <p className="text-xl text-gray-400">Add your Twitter username to enhance your experience.</p>
+            </div>
+            
+            <div className="max-w-sm mx-auto w-full space-y-4 mb-6">
+              <div className="space-y-2">
+                <Label htmlFor="twitter-username" className="text-white font-medium">Twitter Username</Label>
+                <div className="relative">
+                <FaXTwitter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+                <Input 
+                id="twitter-username"
+                type="text" 
+                value={twitterUsername}
+                onChange={(e) => setTwitterUsername(e.target.value)}
+                placeholder="@yourtwitterusername"
+                className="pl-10 bg-black border-gray-700 text-white placeholder:text-gray-500"
+                />
+                </div>
+                {error && <p className="text-red-500 mt-2">{error}</p>}
+              </div>
+            </div>
+
+            <div className="p-1">
             <button 
-            className="bg-white border-1 text-black rounded-md px-4 mt-2"
+            className="bg-blue-600 border-1 hover:bg-blue-700 min-w-[300px] md:min-w-[200px] text-white rounded-md flex justify-center items-center py-3 px-5"
             onClick={handleSubmit}
-            >continue</button>
+            >Continue
+            <ArrowRight className="h-4 w-4 ml-2" />
+            </button>
+            </div>
         </div>
     )
 }

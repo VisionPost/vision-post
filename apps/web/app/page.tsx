6e61ebeb8d03581d4b20e570833ade99ffcfa9e6
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-03-18 17:38:59 +0530

    revert

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-blue-950 text-white">
+    <div className="min-h-screen flex justify-center items-center bg-black text-white">
       <AuthComponent />
     </div>
   );

f8d6cdfc109142fd8f9248f2e90ebeba73073c26
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-03-18 17:23:58 +0530

    minor change

--- apps/web/app/components/authComponent.tsx
+++ apps/web/app/components/authComponent.tsx
@@ -4,7 +4,7 @@ import { signIn, signOut } from "next-auth/react";
 
 export default function AuthComponent() {
   return (
-    <div className="gap-2" >
+    <div className="flex space-x-3" >
       <button onClick={() => signIn()}>Sign in</button>
       <button onClick={() => signOut()}>Sign out</button>
     </div>

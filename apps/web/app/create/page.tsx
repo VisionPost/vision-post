a84cd8a87bb65f0210840f5169eab215328d2276
Govind vashishat <137113082+govind-vashishat@users.noreply.github.com>
2025-04-08 00:18:54 +0530

    minor changes

--- apps/web/app/create/page.tsx
+++ apps/web/app/create/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
+
 
 export default async function Create() {
     return (
-        <div className="min-h-screen text-slate-300">
+        <div className="min-h-screen text-slate-200">
             <header className="border-b border-gray-800 py-3 px-4">
                 <div className="container flex items-center justify-between">
                     <div className="flex items-center gap-8">
-                       <h1 className="text-xl ">VisionPost</h1> 
+                       <h1 className="text-xl font-bold ml-12">VisionPost</h1> 
                     </div>
                 </div>
             </header>
